914b8c59237cd990341f9ee79837bc15b8ff933f
0xwildhare <sschell1979@gmail.com>
2024-11-16 05:44:23 -0800

    add: eas stuff

--- src/vlayer/SimpleTeleportProver.sol
+++ src/vlayer/SimpleTeleportProver.sol
@@ -27,7 +27,7 @@ contract SimpleTeleportProver is Prover {
         registryCoordinator = IRegistryCoordinator(0xeCd099fA5048c3738a5544347D8cBc8076E76494);
     }
 
-    function checkSignature(BLSSigSchema memory schema) public returns (Proof memory, bool success) {
+    function checkSignature(BLSSigSchema memory schema) public returns (Proof memory) {
         setChain(1, 21199991);
         BN254.G1Point memory apk = BN254.G1Point(0, 0);
         for (uint256 i = 0; i < schema.nonSignerPubkeys.length; i++) {
@@ -49,7 +49,11 @@ contract SimpleTeleportProver is Prover {
             trySignatureAndApkVerification(schema.msgPoint, apk, schema.apkG2, schema.sigma);
         assert(pairingSuccessful && siganatureIsValid);
 
-        return (proof(), pairingSuccessful && siganatureIsValid);
+        return (proof());function onAttest(Attestation calldata attestation, uint256 /*value*/ ) internal view override returns (bool) {
+        require(msg.sender == address(_eas), "OpacityResolver: Only EAS can call this function");
+        Proof memory proof = abi.decode(attestation, (Proof));
+        return verify(proof);
+    }
     }
 
     function trySignatureAndApkVerification(
--- src/vlayer/SimpleTeleportVerifier.sol
+++ src/vlayer/SimpleTeleportVerifier.sol
@@ -1,31 +1,65 @@
+// // SPDX-License-Identifier: UNLICENSED
+// pragma solidity ^0.8.23;
+
+// import {SimpleTeleportProver} from "./SimpleTeleportProver.sol";
+// import {WhaleBadgeNFT} from "./WhaleBadgeNFT.sol";
+
+// import {Proof} from "vlayer-0.1.0/Proof.sol";
+// import {Verifier} from "vlayer-0.1.0/Verifier.sol";
+
+// contract SimpleTeleportVerifier is Verifier {
+//     address public prover;
+//     mapping(address => bool) public claimed;
+//     WhaleBadgeNFT public reward;
+
+//     constructor(address _prover, WhaleBadgeNFT _nft) {
+//         prover = _prover;
+//         reward = _nft;
+//     }
+
+//     function claim(Proof calldata) public onlyVerified(prover, SimpleTeleportProver.checkSignature.selector) {
+//         address claimer = msg.sender;
+//         require(!claimed[claimer], "Already claimed");
+//         claimed[claimer] = true;
+//         reward.mint(claimer);
+//     }
+// }
+
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.23;
 
 import {SimpleTeleportProver} from "./SimpleTeleportProver.sol";
-import {WhaleBadgeNFT} from "./WhaleBadgeNFT.sol";
+import {IEAS, Attestation} from "@eas-contracts/IEAS.sol";
+import {SchemaResolver} from "@eas-contracts/resolver/SchemaResolver.sol";
 
 import {Proof} from "vlayer-0.1.0/Proof.sol";
 import {Verifier} from "vlayer-0.1.0/Verifier.sol";
 
 contract SimpleTeleportVerifier is Verifier {
     address public prover;
-    mapping(address => bool) public claimed;
-    WhaleBadgeNFT public reward;
 
-    constructor(address _prover, WhaleBadgeNFT _nft) {
+    constructor(address _prover, address easAddrs) SchemaResolver(IEAS(easAddrs)) {
         prover = _prover;
-        reward = _nft;
     }
 
-    function claim(Proof calldata, bool success)
+    function verify(Proof calldata)
         public
         onlyVerified(prover, SimpleTeleportProver.checkSignature.selector)
+        returns (bool)
     {
-        address claimer = msg.sender;
-        require(!claimed[claimer], "Already claimed");
-        if (success) {
-            claimed[claimer] = true;
-            reward.mint(claimer);
-        }
+        return true;
+    }
+
+    function onAttest(Attestation calldata attestation, uint256 /*value*/ ) internal view override returns (bool) {
+        require(msg.sender == address(_eas), "OpacityResolver: Only EAS can call this function");
+        Proof memory proof = abi.decode(attestation, (Proof));
+        return verify(proof);
+    }
+
+    function onRevoke(Attestation calldata, /*attestation*/ uint256 /*value*/ ) internal view override returns (bool) {
+        require(msg.sender == address(_eas), "OpacityResolver: Only EAS can call this function");
+
+        // Does not support revocation.
+        return false;
     }
 }
